00295de0f044f83e8e7e8199849081f5459ce780
karmise <yucant3@gmail.com>
2025-08-06 20:44:30 +0300

    Add tests

--- homework_1/tests/test_smoke.py
+++ homework_1/tests/test_smoke.py
@@ -4,9 +4,19 @@ import requests
 
 
 def test_status_endpoint(app_url):
-    response = requests.get(f"{app_url}/api/status")
+    response = requests.get(f"{app_url}/status")
     body = response.json()
 
     assert response.status_code == HTTPStatus.OK
     assert "users" in body
     assert isinstance(body["users"], bool)
+
+
+def test_smoke_get_users(app_url):
+    response = requests.get(f"{app_url}/api/users")
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_smoke_get_user_by_id(app_url):
+    response = requests.get(f"{app_url}/api/users/1")
+    assert response.status_code in (HTTPStatus.OK, HTTPStatus.NOT_FOUND)
